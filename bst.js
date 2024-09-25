d8b00a91ec27a8807620ad6d2267fca84e1511b9
Iulian Nichifor <73722884+Seven719@users.noreply.github.com>
2024-09-25 14:40:19 +0100

    added buildTree method
    imported Node from node.js

--- bst.js
+++ bst.js
@@ -1,6 +1,21 @@
+import Node from "./node.js";
+
 export default class Tree {
   constructor(array) {
     const sortedArray = [...new Set(array)].sort((a, b) => a - b);
     this.root = this.buildTree(sortedArray);
+  }
+
+  buildTree(array) {
+    if (array.length === 0) return null;
+
+    const middle = Math.floor(array.length / 2);
+
+    const node = new Node(array[middle]);
+
+    node.left = this.buildTree(array.slice(0, middle));
+    node.right = this.buildTree(array.slice(middle + 1));
+
+    return node;
   }
 }
